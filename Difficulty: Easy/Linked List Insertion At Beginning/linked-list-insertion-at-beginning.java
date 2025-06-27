b22fc3279edfbf20ad16cc50727ca14d8d20cda5
Navyamathan <navyamathan@gmail.com>
2025-06-27 13:17:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked List Insertion At Beginning/linked-list-insertion-at-beginning.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+
+class Solution {
+    public Node insertAtBegining(Node head, int x) 
+    {
+        Node newNode = new Node(x);
+        newNode.next = head;
+        return newNode;
+    }
+}
